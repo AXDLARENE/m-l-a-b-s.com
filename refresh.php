75f67c55401fba45d37b7a0843ba7c21fce3ff90
AXDLARENE <30471865+AXDLARENE@users.noreply.github.com>
2017-07-26 13:46:24 -0300

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -3,7 +3,8 @@ $url = 'https://graph.facebook.com/v2.9';
 $fields = array(
             'id'=>urlencode('http://m-l-a-b-s.com'),
             'scrape'=>urlencode(true),
-            'access_token'=>"<coloque_seu_token_do_aplicativo_facebook_aqui>"
+            'access_token'=>"<	
+768431556662483|DL5Qv3jjsQINrobP0U8ov7uUyDw>"
         );
 
 $fields_string = '';
